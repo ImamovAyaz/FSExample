4d86e6d03dbfc269a44b9f771f631eeb3baff584
Imamov Ayaz <ayaz.imamov.99@mail.ru>
2023-04-21 16:29:27 +0300

    Refactoring Calculator

--- TestCalculator.java
+++ TestCalculator.java
@@ -1,89 +1,70 @@
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import java.security.InvalidParameterException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCalculator {
     private final Calculator calculator = new Calculator();
 
     @Test
-    void addTest(){
-        assertEquals(2, calculator.add(1,1));
+    void addTest() {
+        assertEquals(2, calculator.add(1, 1));
     }
 
     @Test
-    void addToResult(){
-        calculator.add(1,1);
-        assertEquals(5,calculator.addToResult(3));
+    void addToResult() {
+        calculator.add(1, 1);
+        assertEquals(5, calculator.addToResult(3));
+    }
+    @Test
+    void subTest() {
+        assertEquals(0, calculator.sub(1, 1));
     }
 
     @Test
-    void addNegativeTest(){
-        assertNotEquals(3, calculator.add(1,1));
-    }
-
-    @Test
-//    @Tag("Негативыный тест")
-    void subTest(){
-        assertEquals(0, calculator.sub(1,1));
-    }
-
-    @Test
-    void subToResult(){
-        calculator.sub(5,1);
+    void subToResult() {
+        calculator.sub(5, 1);
         assertEquals(1, calculator.subToResult(3));
     }
 
     @Test
-    void subNegativeTest(){
-        assertNotEquals(1, calculator.sub(1,1));
+    void mulTest() {
+        assertEquals(20, calculator.mul(4, 5));
     }
 
     @Test
-    void mulTest(){
-        assertEquals(20, calculator.mul(4,5));
-    }
-
-    @Test
-    void mulToResult(){
-        calculator.mul(5,2);
+    void mulToResult() {
+        calculator.mul(5, 2);
         assertEquals(30, calculator.mulToResult(3));
     }
 
     @Test
-    void mulNegativeTest(){
-        assertNotEquals(25, calculator.mul(4,5));
+    void divTest() {
+        assertEquals(5, calculator.div(10, 2));
     }
 
     @Test
-    void divTest(){
-        assertEquals(5, calculator.div(10,2));
-    }
-
-    @Test
-    void divToResult(){
-        calculator.div(30,2);
+    void divToResult() {
+        calculator.div(30, 2);
         assertEquals(5, calculator.divToResult(3));
     }
 
-    @Test
-    void divNegativeTest(){
-        assertNotEquals(10, calculator.div(10,2));
+    @Test()
+    void divNegativeTest() {
+        assertThrows(InvalidParameterException.class, () -> {
+            calculator.div(10, 0);
+        },"Было вызвано исключение InvalidParameterException");
     }
 
     @Test
-    void powTest(){
-        assertEquals(8, calculator.pow(2,3));
+    void powTest() {
+        assertEquals(8, calculator.pow(2, 3));
     }
 
     @Test
-    void powToResult(){
-        calculator.pow(2,2);
+    void powToResult() {
+        calculator.pow(2, 2);
         assertEquals(16, calculator.powToResult(2));
-    }
-
-    @Test
-    void powTNegativeTest(){
-        assertNotEquals(22, calculator.pow(2,3));
     }
 }
